28748eada247ac035bdebf4905a51ca83304808f
Zephyrus011 <ritvikgupta011@gmail.com>
2022-09-27 21:00:55 -0400

    added powerplay map

--- src/Components/FieldMap.js
+++ src/Components/FieldMap.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Map from "./Map";
 import Card from '@mui/material/Card';
 
 function FieldMap() {
   return (
     <Card variant="outlined">
       <h2>Field Map</h2>
+      <Map />
     </Card>
   );
 }
---
+++ src/Components/Map.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Map() {
+  return (
+    <div>
+      <img src="field.png" alt="field"></img>
+    </div>
+  );
+}
+
+export default Map;
